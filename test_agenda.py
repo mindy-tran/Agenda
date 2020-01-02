e70d3edb9511e16bc7a40b1d66f1d0a3a7c15eb3
Michal Young <michal@cs.uoregon.edu>
2020-01-02 15:47:01 -0800

    Revised for 2020:  Wrapping rather than extending

--- test_agenda.py
+++ test_agenda.py
@@ -6,49 +6,6 @@ logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-
-class TestAppt(unittest.TestCase):
-    """Basic Unit Tests for Appt class"""
-
-    def setUp(self):
-        self.coffee = parse_appt("2019-01-01 08:00 10:00 | Coffee with neighbors")
-        self.coffee_same = parse_appt("2019-01-01 08:00 10:00 | Should be same as coffee")
-        self.brunch = parse_appt("2019-01-01 09:00  13:00 | Brunch with neighbors")
-        self.brunch_same = parse_appt("2019-01-01 09:00  13:00 | Should be same as brunch")
-        self.more_coffee = parse_appt("2019-01-01 13:00 15:00 | More coffee")
-        self.naptime = parse_appt("2019-01-01 16:00 18:00 | Afternoon nap")
-        self.last_year = parse_appt("2018-01-01 09:00  13:00 | Brunch with neighbors")
-        self.next_month = parse_appt("2019-02-01 09:00  13:00 | Brunch with neighbors")
-
-    def test_00_equality(self):
-        self.assertTrue(self.brunch == self.brunch)
-        self.assertTrue(self.coffee == self.coffee_same)
-        self.assertTrue(self.coffee != self.brunch)
-
-    def test_01_order(self):
-        self.assertTrue(self.coffee < self.more_coffee)
-        self.assertFalse(self.coffee > self.more_coffee)
-        self.assertTrue(self.more_coffee > self.coffee)
-        self.assertFalse(self.more_coffee < self.coffee)
-        self.assertTrue(self.last_year < self.brunch)
-        self.assertTrue(self.brunch < self.next_month)
-        self.assertTrue(self.naptime > self.more_coffee)
-
-    def test_02_overlap(self):
-        self.assertTrue(self.coffee.overlaps(self.brunch))
-        self.assertTrue(self.brunch.overlaps(self.coffee))
-        self.assertFalse(self.coffee.overlaps(self.more_coffee))
-        self.assertFalse(self.brunch.overlaps(self.more_coffee))
-
-    def test_03_intersect(self):
-        coffee_at_brunch = self.coffee.intersect(self.brunch)
-        self.assertEqual(coffee_at_brunch,
-                         parse_appt("2019-01-01 09:00 10:00 | Title doesn't matter"))
-        # Original appointments should be unmodified
-        self.assertEqual(self.coffee, self.coffee_same)
-        self.assertEqual(self.brunch, self.brunch_same)
-
-
 def crush(s: str) -> str:
     """Remove whitespace.  Useful in comparison.
     Note, does not remove comments.
@@ -102,7 +59,7 @@ class TestAgendaConflicts(unittest.TestCase):
         2018-01-01 11:30 12:00 | waking
         """
         small_conflicts = """
-        2018-01-01 10:15 10:30 |  drowsy and  coffee
+        2018-01-01 10:15 10:30 |  drowsy & coffee
         """
         self.small = parse_agenda(small)
         self.small_conflicts = parse_agenda(small_conflicts)
@@ -112,7 +69,8 @@ class TestAgendaConflicts(unittest.TestCase):
 
     def test_0_conflict(self):
         """A small agenda that does have a conflict"""
-        self.assertEqual(self.small.conflicts(), self.small_conflicts)
+        conflicts = self.small.conflicts()
+        self.assertEqual(conflicts, self.small_conflicts)
 
     def test_1_fast(self):
         """A linear time algorithm should be able to test
@@ -120,7 +78,7 @@ class TestAgendaConflicts(unittest.TestCase):
         even on a fairly slow computer.
         """
         time_before = time.perf_counter()
-        self.assertTrue(self.big.unconflicted())
+        self.assertTrue(len(self.big.conflicts()) == 0)
         time_after = time.perf_counter()
         elapsed_seconds = time_after - time_before
         self.assertLess(elapsed_seconds, 2, "Are you sure your algorithm is linear time?")
